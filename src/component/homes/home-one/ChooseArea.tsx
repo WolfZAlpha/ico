96b2d251244fcc28b8fb8b442fcc7193fcb2bb06
WolfZAlpha <137004993+WolfZAlpha@users.noreply.github.com>
2024-05-25 13:41:45 -0700

    not12

--- src/component/homes/home-one/ChooseArea.tsx
+++ src/component/homes/home-one/ChooseArea.tsx
@@ -92,7 +92,7 @@ const ChooseArea = () => {
                   <div className="col-lg-7">
                      <div className="section-title mb-0">
                         <h2 className="title style2">Why Choose $PROS?</h2>
-                        <p className="sec-text">Choose $PROS token to harness the innovative power of DeFi, <br /> securing your financial future with reliable dividends and a sustainable, <br />growth-focused ecosystem 
+                        <p className="sec-text">Choose $PROS token to harness the innovative power of DeFi, securing your financial future with reliable dividends and a sustainable, growth-focused ecosystem. 
                         </p>
                      </div>
                   </div>
